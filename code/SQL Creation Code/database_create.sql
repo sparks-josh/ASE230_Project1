bf484f703d07b8c8e9cdc3a9e17393b9bbcfd34e
sparksj10 <sparksj10@nku.edu>
2025-10-13 19:14:09 -0400

    test

--- code/SQL Creation Code/database_create.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (id INT PRIMARY KEY, name VARCHAR(20));
-
-CREATE TABLE boards (id INT PRIMARY KEY, name VARCHAR(20));
-
-CREATE TABLE issues (id INT PRIMARY KEY, name VARCHAR(20), board_id INT REFERENCES boards(id), status VARCHAR(20));
-
-CREATE TABLE comments (id INT PRIMARY KEY, issue_id INT REFERENCES issues(id), comment VARCHAR(100)); 
